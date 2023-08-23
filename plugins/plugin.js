bae8e2255662ce0103f45573e1e9c02cc2d5d899
Eypz Satânico <105191630+sataniceypz@users.noreply.github.com>
2023-08-23 08:49:48 +0530

    Update plugin.js

--- plugins/plugin.js
+++ plugins/plugin.js
@@ -30,7 +30,7 @@ Function({
 	desc: 'Remove a specific plugin or all plugins',
 	type: 'user'
 }, async (message, match, client) => {
-	if (!match) return await message.reply('*Need plugin name!*\n_Example :_\n.remove mforward\n.remove all');
+	if (!match) return await message.reply('*which plugin? enter plugin name !*\n_Example :_\n.remove mforward\n.remove all');
 	try {
 		if (match.toLowerCase() === 'all') {
 			const plugins = await PluginDB.PluginDB.findAll();
@@ -39,7 +39,7 @@ Function({
 				await removeCommand(pluginName);
 				await plugin.destroy();
 			}
-			return await message.reply('_All plugins successfully deleted!_\n*Reboot the BOT*');
+			return await message.reply('_ successfully deleted!_\n*Reboot the BOT*');
 		}
 		const plugin = await PluginDB.PluginDB.findAll({ where: { name: match } });
 		if (plugin.length < 1) return await message.reply(`Plugin *${match}* not found.`);
@@ -47,7 +47,7 @@ Function({
 		await plugin[0].destroy();
 		delete require.cache[require.resolve(`./${match}.js`)];
 		fs.unlinkSync(`./plugins/${match}.js`);
-		await message.reply('_Plugin successfully deleted!_\n*Reboot the BOT*');
+		await message.reply('_ successfully deleted!_\n*Reboot the BOT*');
 	} catch (error) {
 		console.error(error);
 		await message.reply('An error occurred while removing the plugin(s).\n*Error:' + error.message + '*');
